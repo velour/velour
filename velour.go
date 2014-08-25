8b0077e4440ac36557c61babc87a85f5a3b4b6a0
Steve McCoy <mccoyst@gmail.com>
2014-08-25 19:14:38 -0400

    Distruct certs by default.

--- velour.go
+++ velour.go
@@ -44,7 +44,7 @@ var (
 	util  = flag.String("u", "", "utility program")
 	join  = flag.String("j", "", "automatically join a channel")
 	ssl   = flag.Bool("ssl", false, "use SSL to connect to the server")
-	verifySsl = flag.Bool("verify", false, "verify server's SSL certificate")
+	trustSsl = flag.Bool("trust", false, "don't verify server's SSL certificate")
 )
 
 var (
@@ -155,7 +155,7 @@ func connect(addr string) <-chan bool {
 		for {
 			var err error
 			if *ssl {
-				client, err = irc.DialSSL(addr, *nick, *full, *pass, !*verifySsl)
+				client, err = irc.DialSSL(addr, *nick, *full, *pass, *trustSsl)
 			} else {
 				client, err = irc.Dial(addr, *nick, *full, *pass)
 			}
